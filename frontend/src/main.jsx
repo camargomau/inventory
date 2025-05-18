118080b37c7ac2de6eb3854f592969a3a47a4086
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2025-05-18 17:34:09 -0600

    feat: add Inter font dependency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
+import '@fontsource/inter/index.css';
 import '@mantine/core/styles.css';
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        primaryColor: "cyan",
+        defaultRadius: "md",
+        fontFamily: "Inter, sans-serif",
+      }}
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>
