0fd7ff629d7e985c007288ec9a7fd8177dd5d3c6
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2025-05-18 20:48:10 -0600

    style: comment auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+// Login user with email and password
 export async function login(email, password) {
   const { data } = await axios.post(`${API_URL}/api/auth/login`, { email, password });
   return data;
 }
 
+// Register a new user with username, email, and password
 export async function register(username, email, password) {
   const { data } = await axios.post(`${API_URL}/api/auth/register`, { username, email, password });
   return data;
